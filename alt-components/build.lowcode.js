f5ed295eb96b9af2a6707a32763b5f9390d8559b
liurui <499960698@qq.com>
2022-11-04 13:37:03 +0800

    chore: init repo

---
+++ alt-components/build.lowcode.js
@@ -0,0 +1,25 @@
+const { library } = require('./build.json');
+
+module.exports = {
+  alias: {
+    '@': './src',
+  },
+  plugins: [
+    [
+      '@alifd/build-plugin-lowcode',
+      {
+        library,
+        engineScope: '@alilc',
+      },
+    ],
+    [
+      '@alilc/build-plugin-alt',
+      {
+        type: 'component',
+        library,
+        inject: true,
+        openUrl: 'http://localhost:5556?debug',
+      },
+    ],
+  ],
+};
